8d55a0bab77a0d88d3fe877a6acb77794b61577f
Jorge Cortes <jorge.di.fellatio02@gmail.com>
2024-10-13 00:17:27 +0200

    tarea 9 finalizada

--- PHP/index.php
+++ PHP/index.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,19 +9,22 @@
     <link rel="stylesheet" type="text/css" href="../CSS/styles.css">
     <link href="https://fonts.googleapis.com/css2?family=Cinzel:wght@400;700&display=swap" rel="stylesheet">
 </head>
+
 <body class="page-index">
     <div class="main_container">
         <h1>LOST IN THE SANDS</h1>
-        <p>Endinsa't en el fascinant món de l'antic Egipte en aquest emocionant joc de batalla naval! 
-        Enmig de les misterioses dunes del desert i sota el sol abrasador, hauràs de comandar la teva pròpia flota per enfrontar-te 
-        a un exèrcit de mòmies malèvoles que han despertat de les seves tombes.</p>
-        <p>El teu objectiu és trobar i destruir les hordes de mòmies seleccionant les diferents posicions en què es trobaran. 
-        L'estratègia, la punteria i una mica de sort seran les teves millors aliades en aquesta aventura. 
-        Dispara amb precisió per aconseguir la victòria!</p>
+        <p>Endinsa't en el fascinant món de l'antic Egipte en aquest emocionant joc de batalla naval!
+            Enmig de les misterioses dunes del desert i sota el sol abrasador, hauràs de comandar la teva pròpia flota
+            per enfrontar-te
+            a un exèrcit de mòmies malèvoles que han despertat de les seves tombes.</p>
+        <p>El teu objectiu és trobar i destruir les hordes de mòmies seleccionant les diferents posicions en què es
+            trobaran.
+            L'estratègia, la punteria i una mica de sort seran les teves millors aliades en aquesta aventura.
+            Dispara amb precisió per aconseguir la victòria!</p>
         <div class="button_container">
             <button>PARTIDA CLÀSICA</button>
             <button>HALL OF FAME</button>
-        </div> 
+        </div>
     </div>
 
 </html>
