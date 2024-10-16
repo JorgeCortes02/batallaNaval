4eb959a4715af18ff1f47419b1e88deceef61cc8
JorgeCortes02 <120786819+JorgeCortes02@users.noreply.github.com>
2024-10-16 20:24:12 +0200

    Delete JS directory

--- JS/index.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-
-    let initGameButton = document.getElementById("initGame");
-
-    initGameButton.classList.replace("button-disabled", "buttonActive");
-
-});
