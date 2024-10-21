ee782e901056d8d480c7eaa74c70f4382a2c8307
Pau Gracia <paugracia86@yahoo.com>
2024-10-21 13:35:13 +0200

    para merge

--- win.php
+++ win.php
@@ -11,10 +11,11 @@
 
 </head>
 
-<body class="page-index">
+<body class="page-win">
     <?php
+    //COMENTADO PARA PRUEVAS
     // Verifica si el encabezado HTTP_REFERER está establecido
-    if (!isset($_SERVER['HTTP_REFERER']) || strpos($_SERVER['HTTP_REFERER'], 'game.php') === false) {
+   /* if (!isset($_SERVER['HTTP_REFERER']) || strpos($_SERVER['HTTP_REFERER'], 'game.php') === false) {
         // Si no es referida desde la página del juego, retorna un 403
         header('HTTP/1.1 403 Forbidden');
         echo " <div class='main_container yellowBox'>
@@ -24,16 +25,19 @@
     </div>
 ";
         exit;
-    }
+    }*/
 
     // Aquí va el código de la página de victoria o derrota
-    echo "¡Felicidades! Has ganado el juego.";
+    //echo "¡Felicidades! Has ganado el juego.";
     ?>
 
-    <div class="main_container yellowBox">
+    <div class="main_container-victory">
 
-        <h1>LOST IN THE SANDS</h1>
-        <h1>Felidades has ganado!</h1>
+        <h1>Victoria!</h1>
+        <p>Les teves grans dots tàctiques i la teva extraordinària capacitat de lideratge ens han conduït al més gran dels èxits. 
+        El faraó està profundament orgullós de tu i et recompensarà amb la més gran de les fortunes imaginables. 
+        Ara, et confia la missió sagrada de protegir el seu poble de qualsevol maledicció que pugui venir en el futur, 
+        perquè el regne perduri en la prosperitat i la glòria eterna.</p>
     </div>
 
 </html>
