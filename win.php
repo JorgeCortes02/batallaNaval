e18e7e331379f86839feac3e141b09b75ece1156
Jorge Cortes <jorge.di.fellatio02@gmail.com>
2024-10-21 12:48:29 +0200

    win en proceso

--- win.php
+++ win.php
@@ -1,3 +1,17 @@
+<?php session_start();
+if (isset($_SESSION['name'])) {
+
+    $name = $_SESSION['name'];
+
+} else {
+
+    $name = "Introduce tu nombre.";
+
+}
+
+
+?>
+
 <!DOCTYPE html>
 
 <html lang="ca">
@@ -12,27 +26,29 @@
 </head>
 
 <body class="page-index">
+
     <?php
-    // Verifica si el encabezado HTTP_REFERER está establecido
-    if (!isset($_SERVER['HTTP_REFERER']) || strpos($_SERVER['HTTP_REFERER'], 'game.php') === false) {
-        // Si no es referida desde la página del juego, retorna un 403
-        header('HTTP/1.1 403 Forbidden');
-        echo " <div class='main_container yellowBox'>
 
-        <h1>403 Forbidden: Has de accedir desde Game</h1>
-        
-        </div>";
 
-        exit;
-    } else {
-    }
 
     // Aquí va el código de la página de victoria o derrota
     echo "<div class='main_container yellowBox'>
 
             <h1>LOST IN THE SANDS</h1>
             <h1>Felidades has ganado!</h1>
-        
+            <div id='nameRecord'>
+                        <label for='name'>Allista't, com et dius:</label>
+                        <input type='text' id='nameIndex' name='name' value='$name' required>
+                    </div>
+                    <div id='longUser'>
+                        <h3>El jugador ha de tenir entre 3 i 30 caràcters.</h3>
+                    </div>
+                    <div id='buttonPlay'>
+                    
+                    <button id='saveRecord' class='button-disabled' type='submit'>Guardar record</button>
+
+                    <button id='toButton2' class='buttonActive' type='button' >Index</button>
+                </div>
             </div>";
 
     ?>
