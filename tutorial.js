4ddfc7c2c97283434b2134869bfc0f2a1df98ef7
Jorge Cortes <jorge.di.fellatio02@gmail.com>
2024-10-21 23:35:37 +0200

    crono en game activado

--- tutorial.js
+++ tutorial.js
@@ -32,7 +32,7 @@ function easterEggEvent() {
     // Creation of easter egg div which will contain elements
     const easterEggBox = document.getElementById('easterEggMessageBox');
     easterEggBox.style.display = "flex";
-    
+
     // Add display: none to easterEggBox to close it
     const easterEggCloseButton = document.getElementById('easterEggCloseButton');
     easterEggCloseButton.addEventListener('click', function () {
@@ -44,7 +44,7 @@ function easterEggEvent() {
 
     // Generate sound of the Easter Egg (--> will trigger indiana jones arrayOfSounds[5])
     generateSound("easterEgg");
-    
+
 }
 
 
@@ -124,8 +124,8 @@ function disableTableIfVictory() {
 function turnACell(e) {
     const value = e.target.value; // Get the value of the clicked button
     stateCell = sumFoundPositions(value); // "victory" (for instavictory) This variable will hold the state of the cell (e.g., victory)
-    stateCell = "victory"; 
-    
+
+
     // Change the class from "tableButton" to "button-disabled"
     e.target.classList.replace("tableButton", "button-disabled");
     generateSound(stateCell);
@@ -150,46 +150,42 @@ function turnACell(e) {
 }
 // Function to generate buttons for ranking and home
 function generateRankingAndHomeButtons() {
-    // Get the scoreboard container element
-    let scoreBoard = document.getElementsByClassName("scoreboard")[0];
-
-    // Create a new div for the final game buttons
-    let newDiv = document.createElement("div");
-    newDiv.className = "divButtonsFinalGame";
-
-    // Create the "Home" button
-    let buttonHome = document.createElement("button");
-    buttonHome.innerText = "Inici"; // Set the button text to "Inicio" (Home)
-
-    // Create the "Hall of Fame" button
-    let buttonHall = document.createElement("button");
-    buttonHall.innerText = "Hall of Fame"; // Set the button text
-
-    // Add an event listener to redirect to the home page when clicked
-    buttonHome.addEventListener("click", function () {
-        window.location.href = "index.php"; // Redirect to index.php
-    });
-
-    // Add an event listener to redirect to the ranking page when clicked
-    buttonHall.addEventListener("click", function () {
-        window.location.href = "ranking.php"; // Redirect to ranking.php
-    });
-    // Create the "Guardar Record" button
-    let buttonSaveRecord = document.createElement("button");
-    buttonSaveRecord.innerText = "Guardar Record"; // Set the button text for save record
+    let buttonHome = document.getElementById("buttonHome");
+    let buttonHall = document.getElementById("buttonHall");
+    let buttonSaveRecord = document.getElementById("buttonSaveRecord");
+    let buttonDiv = document.getElementById("divButtonsFinalGame");
 
     // Add an event listener to open the popup for saving record when clicked
-    buttonSaveRecord.addEventListener("click", function () {
-        openModal(); // Llama a la función para abrir el modal
+    // Muestra los botones
+    buttonDiv.style.display = "flex"
+    buttonDiv.style.flexDirection = "row"
+    buttonHome.style.display = "block";
+    buttonHall.style.display = "block";
+    buttonSaveRecord.style.display = "block";
+
+    // Añade funcionalidad a los botones
+
+    // Redirige a la página de inicio cuando se hace clic en el botón "Inici"
+    buttonHome.addEventListener("click", function () {
+        window.location.href = "index.php"; // Redirigir a index.php
     });
 
-    // Append the buttons to the new div
-    newDiv.appendChild(buttonHome);
-    newDiv.appendChild(buttonHall);
-    newDiv.appendChild(buttonSaveRecord);
-    // Append the new div to the scoreboard container
-    scoreBoard.appendChild(newDiv);
+    // Redirige a la página del Hall of Fame cuando se hace clic en el botón "Hall of Fame"
+    buttonHall.addEventListener("click", function () {
+        window.location.href = "ranking.php"; // Redirigir a ranking.php
+    });
+
+    // Abre el modal para guardar el récord cuando se hace clic en "Guardar Record"
+    buttonSaveRecord.addEventListener("click", function () {
+        openModal(); // Llama a la función que abre el modal
+    });
+
+
+
 }
+
+
+
 
 // Function to track the positions found (hits on the ships)
 function sumFoundPositions(positionString) {
