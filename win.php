74b9935b50f51c1aa1c73343253bdf70034225e2
Jorge Cortes <jorge.di.fellatio02@gmail.com>
2024-10-23 20:02:22 +0200

    solucionado errores ia, paso de datos desde index

--- win.php
+++ win.php
@@ -70,7 +70,7 @@ if (isset($_POST['score']) && isset($_POST['score'])) {
 
     <h2>403 Forbidden: Has de accedir desde Game</h2>
     
-    </div>";
+    </div> \n </body> \n</html>";
 
         exit;
     } else {
@@ -104,7 +104,7 @@ if (isset($_POST['score']) && isset($_POST['score'])) {
 
     <!-- Botón de volver a la Landing Page (fuera del formulario) -->
     <div id ='buttonDiv'>
-        <button id='toLanding' class='button'>Volver a la Landing Page</button>
+        <button id='toLanding' class='button'>Tornar a la Landing Page</button>
     </div>
 </div>
 ";
